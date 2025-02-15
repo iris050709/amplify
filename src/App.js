93dcbc536e4dee051139154ffbd2bed775a99f6d
iris050709 <ic8281932@gmail.com>
2025-02-15 08:14:40 -0600

    se genero

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo"  />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          este es nueva info
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          desarrollo en web
-        </a>
+          desarrollo en dev
       </header>
     </div>
   );
